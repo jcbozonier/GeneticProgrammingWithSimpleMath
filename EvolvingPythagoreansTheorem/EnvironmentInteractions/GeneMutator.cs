a678f053a88d08e69dccceb52ff96b9dac5af859
Justin Bozonier <darkxanthos@gmail.com>
2009-11-15 16:14:53 -0800

    Added a gene mutator

---
+++ EvolvingPythagoreansTheorem/EnvironmentInteractions/GeneMutator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvolvingPythagoreansTheorem.EnvironmentInteractions
+{
+    public class GeneMutator
+    {
+        readonly string[] _GeneGrammar;
+        readonly double _MutationProbability;
+
+        public GeneMutator(IEnumerable<string> geneGrammar, double mutationProbability)
+        {
+            _GeneGrammar = geneGrammar.ToArray();
+            _MutationProbability = mutationProbability;
+        }
+
+        public string Mutate(string gene)
+        {
+            var rdm = new Random(DateTime.Now.Millisecond);
+            var chosenNumber = rdm.Next(1, 1000) / 1000.0;
+
+            if (chosenNumber > _MutationProbability)
+                return gene;
+
+            var randomGrammarIndexer = new Random(DateTime.Now.Millisecond);
+            var randomizedMutantGene = 0.Until(gene.Length)
+                    .Select(x => _GeneGrammar[randomGrammarIndexer.Next(0, _GeneGrammar.Length)])
+                    .Aggregate((x, y) => x + y);
+
+            return _Mix(gene, randomizedMutantGene);
+        }
+
+        static string _Mix(string gene, string mutantGene)
+        {
+            var mutatedGene = "";
+            for (var i = 0; i < gene.Length; i++)
+            {
+                mutatedGene += gene[i] + mutantGene[i];
+            }
+
+            return mutatedGene;
+        }
+    }
+}
---
+++ Tests/When_mutating_a_gene.cs
@@ -0,0 +1,77 @@
+﻿using EvolvingPythagoreansTheorem.EnvironmentInteractions;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class When_mutating_a_gene_that_does_mutate
+    {
+        GeneMutator Mutator;
+        string TheGene;
+        string TheMutatedGene;
+
+        [Test]
+        public void It_should_NOT_match_the_gene_offered_for_mutation()
+        {
+            Assert.That(TheMutatedGene, Is.Not.EqualTo(TheGene));
+        }
+
+        [Test]
+        public void It_should_be_twice_as_long()
+        {
+            Assert.That(TheMutatedGene.Length, Is.EqualTo(TheGene.Length * 2));
+        }
+
+        [Test]
+        public void It_should_be_somewhat_randomized()
+        {
+            Assert.That(TheMutatedGene.IndexOf(TheGene), Is.Not.EqualTo(0));
+        }
+
+        [Test]
+        public void It_should_not_have_a_copy_of_itself_at_its_tail()
+        {
+            Assert.That(TheMutatedGene.LastIndexOf(TheGene), Is.Not.EqualTo(3));
+        }
+
+        [SetUp]
+        public void Context()
+        {
+            TheGene = "+xy";
+            Mutator = new GeneMutator(new[] { "+", "-", "/", "*", "r", "a", "b" }, 1);
+            Because();
+        }
+
+        void Because()
+        {
+            TheMutatedGene = Mutator.Mutate(TheGene);
+        }
+    }
+
+    [TestFixture]
+    public class When_mutating_a_gene_that_does_not_mutate
+    {
+        GeneMutator Mutator;
+        string TheGene;
+        string TheMutatedGene;
+
+        [Test]
+        public void It_should_match_the_gene_offered_for_mutation()
+        {
+            Assert.That(TheMutatedGene, Is.EqualTo(TheGene));
+        }
+
+        [SetUp]
+        public void Context()
+        {
+            TheGene = "+xy";
+            Mutator = new GeneMutator(new []{"+", "-", "/", "*", "r", "a", "b"}, 0);
+            Because();
+        }
+
+        void Because()
+        {
+            TheMutatedGene = Mutator.Mutate(TheGene);
+        }
+    }
+}
